63265b4a0ab196771b9d0ad0623b764c7d3eab1c
Daniel George Holz <dgholz@gmail.com>
2023-10-03 15:54:30 +0100

    Make script testable

--- src/check_version_bump.sh
+++ src/check_version_bump.sh
@@ -4,13 +4,24 @@ exec 19> >( sed -e's/^/::debug::/g' )
 BASH_XTRACEFD=19
 export DEBUG=api
 
-function files_changed() {
-  local pr="$1"
-  gh api repos/"${{ github.repository }}/pulls/$pr/files" --jq .[].filename 2>&19
+files_changed() {
+  local repo="$1"
+  local pr="$2"
+  gh api repos/"$repo"/pulls/"$pr"/files --jq .[].filename 2>&19
 }
 
-echo ::group::Changed files
-files_changed "${{ inputs.pr-number }}" | tee files_changed.log
-echo ::endgroup::
+check_for_changed_files() {
+  echo ::group::Changed files
+  tee files_changed.log
+  echo ::endgroup::
 
-grep -q -f CHANGELOG.md files_changed.log && grep -q 'lib/.*/version[.][^.]*$' files_changed.log
+  grep -q -f CHANGELOG.md files_changed.log && grep -q 'lib/.*/version[.][^.]*$' files_changed.log
+}
+
+main() {
+  local repo="$1"
+  local pr="$2"
+  files_changed "$repo" "$pr" | check_for_changed_files
+}
+
+( return 0 2>/dev/null ) || main "$@"
